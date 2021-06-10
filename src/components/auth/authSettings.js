664f9f9a1d59aea94704e37b079a6eedbda31211
Loganwebb1994 <loganwebb1994@gmail.com>
2021-06-10 13:20:16 -0500

    added basic file structure

---
+++ src/components/auth/authSettings.js
@@ -0,0 +1,7 @@
+export const authApi = {
+  localApiBaseUrl: "http://localhost:8000",
+  endpoint: "users"
+}
+
+// The user id is saved under the key app_user_id in session Storage. Change below if needed!
+export const userStorageKey = "goodwood_user"
